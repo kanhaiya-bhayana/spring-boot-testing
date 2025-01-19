57dc0b7bcb78d89fbd6d23211fb95caca26a5535
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-19 18:14:24 +0530

    Calculator: assertEquals 3rd parameter could be lambda

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -9,7 +9,10 @@ class CalculatorTest {
     @Test
     void integerDivision() {
         Calculator calculator = new Calculator();
+        int expected = 2;
+        int actual = 4/2;
         int result = calculator.integerDivision(4,2);
-        assertEquals(2,result, "The integerDivision() did not produce expected result.");
+        assertEquals(expected, actual,
+                () -> expected + " not equal to " + actual);
     }
 }
