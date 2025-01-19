f893662840fc8af186c8c131e710ead3d946f784
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-19 18:16:56 +0530

    Calculator: Supplier init

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -2,6 +2,8 @@ package com.appsdeveloperblog;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -14,5 +16,8 @@ class CalculatorTest {
         int result = calculator.integerDivision(4,2);
         assertEquals(expected, actual,
                 () -> expected + " not equal to " + actual);
+
+        Supplier<String> lambda = () -> expected + " not equal to " + actual;
+        assertEquals(expected,actual,lambda);
     }
 }
