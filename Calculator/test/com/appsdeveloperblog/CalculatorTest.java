2ca9e159a38ea7403ba35af70ebe89a887a0b53d
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-19 18:51:39 +0530

    Calculator: @DisplayName annotation

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -1,15 +1,20 @@
 package com.appsdeveloperblog;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@DisplayName("Test Math operations in Calculator Class")
 class CalculatorTest {
 
+    // naming convention
+    // test<System Under Test>_<Condition or State Change>_<Expected Result>
+
+    @DisplayName("Test 4/2 = 2")
     @Test
-    void integerDivision() {
+    void testIntegerDivision_FourDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
         int expected = 2;
         int actual = 4/2;
@@ -19,5 +24,10 @@ class CalculatorTest {
 
         Supplier<String> lambda = () -> expected + " not equal to " + actual;
         assertEquals(expected,actual,lambda);
+    }
+
+    @DisplayName("Division by zero")
+    @Test
+    void testIntegerDivision_DivideByZero_ShouldThrowArithmaticException(){
     }
 }
