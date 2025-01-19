39da6ba944380fbd2554db60add988d5be3fb8ae
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2025-01-19 19:17:06 +0530

    Calculator: Methods lifecycle

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -1,7 +1,6 @@
 package com.appsdeveloperblog;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.function.Supplier;
 
@@ -12,11 +11,33 @@ class CalculatorTest {
     // naming convention
     // test<System Under Test>_<Condition or State Change>_<Expected Result>
 
+    Calculator calculator;
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("Executing @BeforeAll method.");
+    }
+    @AfterAll
+    static void cleanUp(){
+        System.out.println("Executing @AfterAll method.");
+    }
+    @BeforeEach
+     void beforeEachTestMethod(){
+        calculator = new Calculator();
+        System.out.println("Executing @BeforeEach method.");
+    }
+
+    @AfterEach
+    void afterEachTestMethod(){
+        calculator = new Calculator();
+        System.out.println("Executing @AfterEach method.");
+    }
+
     @DisplayName("Test 4/2 = 2")
     @Test
     void testIntegerDivision_FourDividedByTwo_ShouldReturnTwo() {
         // Arrange - Given
-        Calculator calculator = new Calculator();
+//        Calculator calculator = new Calculator();
         int expected = 2;
         int actual = 4/2;
         Supplier<String> lambda = () -> expected + " not equal to " + actual;
